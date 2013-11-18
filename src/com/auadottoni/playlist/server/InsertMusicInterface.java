2c6febdc74cc355eaedcfc70a41c45fc69b8169a
Tássio Auad <auadtassio@gmail.com>
2013-11-17 23:40:00 -0200

    MusicInfoFrame e a Interface do comunicação dele com o ServerFrame.

---
+++ src/com/auadottoni/playlist/server/InsertMusicInterface.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.auadottoni.playlist.server;
+
+import com.auadottoni.playlist.model.Music;
+
+/**
+ *
+ * @author auadtassio
+ */
+public interface InsertMusicInterface {
+    public void insertMusic(Music music);
+}
